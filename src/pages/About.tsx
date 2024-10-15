ec1c85f7181a123ec852288fe824a7bf92c36643
Kamran Huseynov <huseynovk355@gmail.com>
2024-10-15 00:29:46 -0700

    feat: add responsive layout to About page, fill About with team information
    - when a photo of a club member is hovered bg shadow appears and short
      information summary appears. The transition is implemented using
      opacity reducing performance impact since opacity only impacts the
      compositing step

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,83 +1,157 @@
-import { Box, Image, Heading, Text } from '@chakra-ui/react';
-import vrImage from '../assets/vr-image-square.jpg';
-import blankProfilePicture from '../assets/blank-profile-picture.png';
-import { theme } from './../main';
-import uniqid from 'uniqid';
+import { Box, Heading, Text, Grid, GridItem } from "@chakra-ui/react";
+import kamran from "/img/people/kamran.jpg";
+import alex from "/img/people/alex.jpg";
+import bradley from "/img/people/bradley.jpg";
+import hetav from "/img/people/hetav.jpg";
+import isha from "/img/people/isha.jpeg";
+import lia from "/img/people/lia.png";
+import oliver from "/img/people/oliver.jpg";
+import ryan from "/img/people/ryan.jpg";
+import vivian from "/img/people/vivian.png";
+import uniqid from "uniqid";
 
 export default function About() {
 	const team = [
 		{
-			name: 'John Doe',
-			role: 'President',
-			src: blankProfilePicture,
+			name: "Lia Tulchinsky",
+			role: "Founder and President",
+			src: lia,
 		},
 		{
-			name: 'Jane Doe',
-			role: 'Vice President',
-			src: blankProfilePicture,
+			name: "Kamran Huseynov",
+			role: "VP of Infrastructure",
+			src: kamran,
+		},
+		{
+			name: "Oliver Luo",
+			role: "VP of Engineering",
+			src: oliver,
+		},
+		{
+			name: "Ryan Saloff",
+			role: "VP of Logistics",
+			src: ryan,
+		},
+		{
+			name: "Vivian Weng",
+			role: "Software Developer",
+			src: vivian,
+		},
+		{
+			name: "Alexander Zhong",
+			role: "Software Developer",
+			src: alex,
+		},
+		{
+			name: "Bradley Eugene",
+			role: "Software Developer",
+			src: bradley,
+		},
+		{
+			name: "Isha Dau",
+			role: "Logistics Coordinator",
+			src: isha,
+		},
+		{
+			name: "Hetav ...",
+			role: "Advisor",
+			src: hetav,
 		},
 	];
 
 	return (
-		<Box color='white' display='flex' flexFlow='column' gap='2rem'>
-			<Heading textAlign='center' size='2xl' fontFamily={theme.fonts.pixel}>
-				About Us
-				<Box
-					p='1px'
-					backgroundColor={theme.colors.pink.main}
-					rounded='20px'
-					marginTop='.5rem'
-				/>
-			</Heading>
-			<Box display='flex' justifyContent='center' width='100%'>
-				<Image
-					src={vrImage}
-					objectFit='cover'
-					borderRadius='full'
-					maxWidth='500px'
-					width='80%'
-				/>
-			</Box>
-
-			<Text fontSize='1.2rem'>
-				The purpose of AMS AI Club at UBC is to teach students from all
-				backgrounds how to use AI to leverage their degrees and become
-				proficient in AI use. As AI continues to advance we want our club
-				members to stay ahead of the curve and be confident in their future.
-				That's why we aim to provide bi weekly meetings that involve workshops
-				and discussions on AI developments. Our workshops are designed to give
-				students the tools to build their own projects using AI. These projects
-				can be showcased at the end of the term and will be a great addition to
-				anyone's resume. On top of the workshops, we will also have speakers who
-				work and engage with AI so our members will be able to ask questions and
-				learn from industry professionals. By joining our club you will have an
-				amazing opportunity to network, build AI proficiency and stay the most
-				up to date with AI advancements.
-			</Text>
-
-			<Heading textAlign='center' size='2xl'>
-				Meet The Team
-				<Box
-					p='1px'
-					backgroundColor={theme.colors.pink.main}
-					rounded='20px'
-					marginTop='.5rem'
-				/>
-			</Heading>
-
-			{team.map(({ name, role, src }) => (
-				<Box
-					display='flex'
-					flexFlow='column'
-					alignItems='center'
-					gap='.5em'
-					key={uniqid()}
+		<Grid
+			gap="4em"
+			color="white"
+			px="1.5em"
+			py="2em"
+		>
+			<Grid gap="2em">
+				<Heading
+					textAlign="center"
+					size="2xl"
 				>
-					<Image src={src} objectFit='cover' maxWidth='500px' width='80%' />
-					<Heading textAlign='center'>{name}</Heading>
-					<Text textAlign='center'>{role}</Text>
-				</Box>
-			))}
-		</Box>
+					About Us
+				</Heading>
+
+				<Text>
+					The purpose of AMS AI Club at UBC is to teach students from all backgrounds how to use AI
+					to leverage their degrees and become proficient in AI use. As AI continues to advance we
+					want our club members to stay ahead of the curve and be confident in their future. That's
+					why we aim to provide bi weekly meetings that involve workshops and discussions on AI
+					developments. Our workshops are designed to give students the tools to build their own
+					projects using AI. These projects can be showcased at the end of the term and will be a
+					great addition to anyone's resume. On top of the workshops, we will also have speakers who
+					work and engage with AI so our members will be able to ask questions and learn from
+					industry professionals. By joining our club you will have an amazing opportunity to
+					network, build AI proficiency and stay the most up to date with AI advancements.
+				</Text>
+			</Grid>
+
+			<Grid gap="2em">
+				<Heading
+					textAlign="center"
+					size="2xl"
+				>
+					Meet The Team
+				</Heading>
+
+				<Grid
+					gridTemplateColumns={{
+						base: "repeat(auto-fill, minmax(250px, 1fr))",
+						lg: "repeat(auto-fill, minmax(380px, 1fr))",
+					}}
+				>
+					{team.map(({ name, role, src }) => (
+						<GridItem key={uniqid()}>
+							<Box
+								bgImg={src}
+								aspectRatio={1 / 1}
+								objectFit="cover"
+								bgPos="center"
+								bgSize="cover"
+								bgRepeat="no-repeat"
+								position="relative"
+								sx={{
+									"&::after": {
+										content: "''",
+										position: "absolute",
+										height: "100%",
+										width: "100%",
+										top: 0,
+										left: 0,
+										boxShadow: "inset 0px -120px 110px 0px var(--chakra-colors-purple-900)",
+										opacity: 0,
+										transition: "opacity 0.3s ease-in-out",
+									},
+									"&:hover::after": {
+										opacity: 1,
+									},
+									"& .content": {
+										opacity: 0,
+										transition: "opacity 0.3s ease-in-out",
+									},
+									"&:hover .content": {
+										opacity: 1,
+									},
+								}}
+								display="flex"
+								flexFlow="column"
+								justifyContent="end"
+								p="1rem"
+							>
+								<Box
+									className="content"
+									zIndex={2}
+								>
+									<Heading width="min-content">{name}</Heading>
+									<Text>{role}</Text>
+								</Box>
+							</Box>
+						</GridItem>
+					))}
+				</Grid>
+			</Grid>
+		</Grid>
 	);
 }
