2101616a84b984c151e72a38bc1ed0e141787a07
Kamran Huseynov <huseynovk355@gmail.com>
2024-10-15 00:26:49 -0700

    feat: implement responsive layout in the Main page

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,51 +1,91 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
-import { theme } from './../main';
+import { Grid, GridItem, Heading, Text } from "@chakra-ui/react";
+
+const content = [
+	{
+		heading: "Workshops",
+		text: "Our workshops teach members how to train and fine tune ML (Machine Learning) models. Through the workshop process, students will build a project to show off during a project showcase.",
+		gridArea: "a",
+	},
+	{
+		heading: "Speaker Events",
+		text: "Our workshops teach members how to train and fine tune ML (Machine Learning) models. Through the workshop process, students will build a project to show off during a project showcase.",
+		gridArea: "b",
+	},
+	{
+		heading: "Discussion Events",
+		text: "Our workshops teach members how to train and fine tune ML (Machine Learning) models. Through the workshop process, students will build a project to show off during a project showcase.",
+		gridArea: "c",
+	},
+	{
+		heading: "Competitions",
+		text: "Our workshops teach members how to train and fine tune ML (Machine Learning) models. Through the workshop process, students will build a project to show off during a project showcase.",
+		gridArea: "d",
+	},
+];
 
 export default function Content() {
 	return (
-		<Box display='flex' flexFlow='column' textColor='white' gap='1.5em'>
-			<Box>
-				<Heading fontFamily={theme.fonts.pixel}>Workshops</Heading>
-				<Text>
-					Our workshops teach members how to train and fine tune ML (Machine
-					Learning) models. Through the workshop process, students will build a
-					project to show off during a project showcase.
-				</Text>
-			</Box>
-			<Box
-				textAlign='right'
-				bgColor={theme.colors.pink.main}
-				mx='-1.5rem'
-				padding='1.5rem'
-			>
-				<Heading fontFamily={theme.fonts.pixel}>Speaker Events</Heading>
-				<Text>
-					Our workshops teach members how to train and fine tune ML (Machine
-					Learning) models. Through the workshop process, students will build a
-					project to show off during a project showcase.
-				</Text>
-			</Box>
-			<Box>
-				<Heading fontFamily={theme.fonts.pixel}>Discussion Events</Heading>
-				<Text>
-					Our workshops teach members how to train and fine tune ML (Machine
-					Learning) models. Through the workshop process, students will build a
-					project to show off during a project showcase.
-				</Text>
-			</Box>
-			<Box
-				textAlign='right'
-				bgColor={theme.colors.pink.main}
-				mx='-1.5rem'
-				padding='1.5rem'
-			>
-				<Heading fontFamily={theme.fonts.pixel}>Competitions</Heading>
-				<Text>
-					Our workshops teach members how to train and fine tune ML (Machine
-					Learning) models. Through the workshop process, students will build a
-					project to show off during a project showcase.
-				</Text>
-			</Box>
-		</Box>
+		<Grid
+			templateColumns={{ base: "1fr", lg: "1fr 1fr" }}
+			templateRows={{ base: "repeat(4, min-content)", lg: "1fr 1fr" }}
+			templateAreas={{ base: `"a" "b" "c" "d"`, lg: `"a b" "d c"` }}
+			// gap={{ base: "1.5rem", lg: "0" }}
+			position="relative"
+		>
+			{content.map((item, index) => {
+				const isPink = index % 2 === 0;
+				let props: Object = {};
+				if (isPink) {
+					props = {
+						...props,
+						textAlign: { base: "right", lg: "left" },
+						bgColor: "pink.500.100",
+					};
+				}
+
+				return (
+					<GridItem
+						key={index}
+						area={item.gridArea}
+						p="1.5rem"
+						{...props}
+					>
+						<Heading>{item.heading}</Heading>
+						<Text>{item.text}</Text>
+					</GridItem>
+				);
+			})}
+		</Grid>
 	);
 }
+
+// function AnimatedContentBlock({
+// 	children,
+// 	isOnTheRight,
+// }: {
+// 	children: React.ReactNode;
+// 	isOnTheRight: boolean;
+// }) {
+// 	const boxRef = useRef(null);
+// 	const { scrollYProgress, scrollY } = useScroll({
+// 		target: boxRef,
+// 		offset: ["end end", "start start"],
+// 	});
+
+// 	return (
+// 		<FramerBox
+// 			ref={boxRef}
+// 			position="relative"
+// 			left={`${Number(scrollYProgress) * 100}px`}
+// 			{...(isOnTheRight
+// 				? {
+// 						textAlign: "right",
+// 						bgColor: `${theme.colors.pink.main[1]}`,
+// 						mx: "-1.5rem",
+// 						padding: "1.5rem",
+// 					}
+// 				: {})}>
+// 			{children}
+// 		</FramerBox>
+// 	);
+// }
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,24 +1,56 @@
-import { AspectRatio, Box, Heading } from '@chakra-ui/react';
-import Content from '../components/Content';
-import { theme } from '../main';
+import { AspectRatio, Box, Grid, GridItem } from "@chakra-ui/react";
+import Content from "../components/Content";
 
 export default function Root() {
 	return (
-		<>
-			<AspectRatio max-width='100%' ratio={1} bgColor='white'>
-				<Box>SOMETHING 3D</Box>
-			</AspectRatio>
+		<Grid
+			gap="1.5em"
+			position="relative"
+		>
+			<Box
+				px="1.5em"
+				py="2em"
+			>
+				<Grid
+					gridGap="1.5em"
+					placeItems={{ base: "center", sm: "center" }}
+					gridTemplateRows={{ base: "1fr min-content", sm: "1fr" }}
+					gridTemplateColumns={{ base: "", sm: "1fr 2fr" }}
+					gridTemplateAreas={{
+						base: `"3d" "signup"`,
+						sm: `"signup 3d"`,
+					}}
+				>
+					<GridItem
+						area={"3d"}
+						maxW="600px"
+						width="100%"
+					>
+						<AspectRatio
+							ratio={1}
+							bgColor="white"
+						>
+							<Box color="black">SOMETHING 3D</Box>
+						</AspectRatio>
+					</GridItem>
 
-			<Box>
-				<Heading color='White' pb='.5em' fontFamily={theme.fonts.pixel}>
-					Latest
-				</Heading>
-				<AspectRatio max-width='100%' ratio={1} bgColor='white'>
-					<Box>AN INSTAGRAM POST</Box>
-				</AspectRatio>
+					<GridItem
+						area={"signup"}
+						maxW={"300px"}
+						alignSelf="end"
+						width="100%"
+					>
+						<AspectRatio
+							ratio={1}
+							bgColor="white"
+						>
+							<Box color="black">Sign Up</Box>
+						</AspectRatio>
+					</GridItem>
+				</Grid>
 			</Box>
 
 			<Content />
-		</>
+		</Grid>
 	);
 }
