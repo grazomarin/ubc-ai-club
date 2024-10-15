0096281e11c5ef0ab8a32438e149a34a43874fec
Kamran Huseynov <huseynovk355@gmail.com>
2024-10-15 00:18:49 -0700

    feat: change color scheme, change router implementation
    - updated the color scheme to match the club colors
    - implemented "extendTheme" in order to properly handle custom css
    - changed router creation method to createHashRouter instead of
      HashRouter component. This enables RR's Data API's
    - decoupled react components inside of App.tsx by shifting layout
      implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,57 @@
-import { Box } from '@chakra-ui/react';
-import { theme } from './main';
-import { Route, Routes } from 'react-router-dom';
-import Root from './pages/Root';
-import About from './pages/About';
-import Navbar from './components/Navbar';
-import Events from './pages/Events';
-import { useEffect } from 'react';
-import WebFont from 'webfontloader';
-import { FramerBox } from './components/Framer';
+import { Box } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 export default function App() {
-	useEffect(() => {
-		WebFont.load({
-			google: {
-				families: ['JetBrains Mono', 'Silkscreen'],
-			},
-		});
-	}, []);
-
-	function backgroundGlowComponent() {
-		const y = Math.floor(Math.random() * screen.height);
-		const x = Math.random() > 0.5 ? -100 : screen.width + 100;
-		const color =
-			Math.random() > 0.5 ? theme.colors.pink.light : theme.colors.blue.light;
-		const minShadowBlur = screen.width * 0.1 < 180 ? 180 : screen.width * 0.18;
-		const minShadowSpread =
-			screen.width * 0.2 < 110 ? 110 : screen.width * 0.07;
-		const maxShadowBlur = screen.width * 0.1 < 300 ? 300 : screen.width * 0.25;
-		const maxShadowSpread =
-			screen.width * 0.2 < 200 ? 200 : screen.width * 0.14;
-
-		return (
-			<FramerBox
-				position='fixed'
-				left={x}
-				top={y}
-				backgroundColor={color}
-				animate={{
-					boxShadow: [
-						`0 0 ${minShadowBlur}px ${minShadowSpread}px ${color}`,
-						`0 0 ${maxShadowBlur}px ${maxShadowSpread}px ${color}`,
-					],
-				}}
-				// @ts-expect-error - framer acting weird
-				transition={{
-					duration: Math.random() * 2 + 1.5,
-					ease: 'easeInOut',
-					repeat: Infinity,
-					repeatType: 'reverse',
-				}}
-			></FramerBox>
-		);
-	}
-
 	return (
-		<Box bgColor='black' minH='100vh' maxW='100vw' position='relative'>
-			{backgroundGlowComponent()}
-			{backgroundGlowComponent()}
-			{backgroundGlowComponent()}
+		<Box
+			bgColor="purple.900"
+			minH="100vh"
+			maxW="100vw"
+			position="relative"
+			color="white"
+		>
 			<Navbar />
-			<Box
-				px='1.5em'
-				py='1em'
-				display='flex'
-				flexFlow='column'
-				gap='1.5em'
-				position='relative'
-			>
-				<Routes>
-					<Route path='/' element={<Root />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/events' element={<Events />} />
-				</Routes>
-			</Box>
+			<Outlet />
 		</Box>
 	);
 }
+
+// TODO: fix inneficient memory usage
+// function BackgroundGlow() {
+// 	const y = Math.floor(Math.random() * screen.height);
+// 	const x = Math.random() > 0.5 ? -100 : screen.width + 100;
+// 	const color = Math.random() > 0.5 ? "pink.light" : "blue.light";
+// 	// const minShadowBlur = screen.width * 0.1 < 180 ? 180 : screen.width * 0.18;
+// 	// const minShadowSpread =
+// 	// 	screen.width * 0.2 < 110 ? 110 : screen.width * 0.07;
+// 	const minShadowBlur = 0;
+// 	const minShadowSpread = 0;
+// 	// const maxShadowBlur = screen.width * 0.1 < 300 ? 300 : screen.width * 0.25;
+// 	// const maxShadowSpread =
+// 	// 	screen.width * 0.2 < 200 ? 200 : screen.width * 0.14;
+// 	const maxShadowBlur = screen.width * 0.1 < 200 ? 200 : screen.width * 0.18;
+// 	const maxShadowSpread = screen.width * 0.2 < 150 ? 150 : screen.width * 0.12;
+
+// 	return (
+// 		<FramerBox
+// 			position="fixed"
+// 			left={x}
+// 			top={y}
+// 			backgroundColor={color}
+// 			animate={{
+// 				boxShadow: [
+// 					`0 0 ${minShadowBlur}px ${minShadowSpread}px ${color}`,
+// 					`0 0 ${maxShadowBlur}px ${maxShadowSpread}px ${color}`,
+// 				],
+// 			}}
+// 			// @ts-expect-error - framer acting weird
+// 			transition={{
+// 				duration: Math.random() * 2 + 3,
+// 				ease: "easeInOut",
+// 				repeat: Infinity,
+// 				repeatType: "reverse",
+// 			}}
+// 		/>
+// 	);
+// }
--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "./App";
+import {
+	createHashRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import Root from "./pages/Root";
+import "@fontsource/silkscreen";
+import "@fontsource-variable/jetbrains-mono";
 
 export const theme = {
-	colors: {
-		blue: {
-			main: '#000b39',
-			light: '#001f6d',
-		},
-		pink: {
-			main: '#9a0081',
-			light: '#c700a8',
-		},
-	},
 	sizes: {
-		default: '2rem',
+		default: "2rem",
 	},
-	fonts: {
-		code: 'JetBrains Mono',
-		pixel: 'Silkscreen',
+	radius: {
+		xl: "20px",
+		l: "18px",
+		m: "16px",
+		s: "14px",
+		xs: "12px",
 	},
 };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const chakraTheme = extendTheme({
+	fonts: {
+		heading: "Silkscreen",
+		body: "JetBrains Mono Variable",
+	},
+	colors: {
+		purple: { 600: "#3e2790", 700: "#301F70", 800: "#231650", 900: "#150d30" },
+		blue: {
+			500: {
+				50: "#1AA3A880",
+				75: "#1AA3A8bf",
+				100: "#1AA3A8",
+			},
+		},
+		pink: {
+			500: {
+				50: "#F5009780",
+				75: "#F50097bf",
+				100: "#F50097",
+			},
+		},
+	},
+	components: {
+		Text: {
+			baseStyle: {
+				fontSize: {
+					base: "s",
+					sm: "lg",
+					lg: "xl",
+				},
+			},
+		},
+	},
+});
+
+const router = createHashRouter(
+	createRoutesFromElements(
+		<Route
+			path="/"
+			Component={App}
+		>
+			<Route
+				index
+				Component={Root}
+			/>
+			<Route
+				path="/about"
+				Component={About}
+			/>
+			<Route
+				path="/events"
+				Component={Events}
+			/>
+		</Route>
+	)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
 	<React.StrictMode>
-		<ChakraProvider resetCSS={true}>
-			<HashRouter>
-				<App />
-			</HashRouter>
+		<ChakraProvider
+			resetCSS={true}
+			theme={chakraTheme}
+		>
+			<RouterProvider router={router} />
 		</ChakraProvider>
 	</React.StrictMode>
 );
